3500b3eb704bfc09bff6b540aecb94cc618eb669
Daniela <dsm17789@gmail.com>
2023-10-07 10:21:29 -0700

    cambio 3

--- bodega/src/main/java/ms/bodega/entity/Producto.java
+++ bodega/src/main/java/ms/bodega/entity/Producto.java
@@ -21,4 +21,66 @@ public class Producto {
 
     @Column(name = "EXISTENCIA")
     private Integer existencia;
+
+    public Producto(Integer id, String nombre, Double precio, String codigo, Integer existencia) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.codigo = codigo;
+        this.existencia = existencia;
+    }
+
+    public Producto() {
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", codigo='" + codigo + '\'' +
+                ", existencia=" + existencia +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Integer getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(Integer existencia) {
+        this.existencia = existencia;
+    }
 }
